7dcbb147cdcba9f5279ba9a59c8dfb708e883df2
dev142857 <genuine5621@gmail.com>
2024-04-29 12:56:30 -0400

    #test

--- src/auth0-provider-with-navigate.js
+++ src/auth0-provider-with-navigate.js
@@ -7,11 +7,18 @@ export const Auth0ProviderWithNavigate = ({ children }) => {
 
   const domain = process.env.REACT_APP_AUTH0_DOMAIN;
   const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
-  const redirectUri = process.env.REACT_APP_AUTH0_CALLBACK_URL;
+  const redirectUri =
+    process.env.REACT_APP_VERCEL_ENV === "production"
+      ? `https://${window.location.hostname}/callback`
+      : process.env.REACT_APP_AUTH0_CALLBACK_URL;
 
   const onRedirectCallback = (appState) => {
     navigate(appState?.returnTo || window.location.pathname);
   };
+  console.log(process.env.REACT_APP_VERCEL_ENV);
+  console.log(domain);
+  console.log(clientId);
+  console.log(redirectUri);
 
   if (!(domain && clientId && redirectUri)) {
     return null;
